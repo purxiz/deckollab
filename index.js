b5ac69a2a4faaee919c1baf7801d944aec062ff4
angelopoole <angelo.poole@outlook.com>
2020-09-10 15:53:47 -0400

    Spaces -> Tabs

--- index.js
+++ index.js
@@ -5,10 +5,10 @@ const favicon = require('serve-favicon');
 const wsh = require('./ws/handler');
 
 mongoose.connect('mongodb://localhost/deckollab', {
-  useNewUrlParser: true,
-  useUnifiedTopology: true,
-  useCreateIndex: true,
-  useFindAndModify: false,
+	useNewUrlParser: true,
+	useUnifiedTopology: true,
+	useCreateIndex: true,
+	useFindAndModify: false,
 });
 
 var express = require('express');
@@ -34,12 +34,12 @@ app.use('/api/card_selector', require('./api/card_selector'));
 
 // 404 page on bad url response, keep at end of page.
 app.use(function (req, res, next) {
-  res.status(404);
-  // respond with html page
-  if (req.accepts('html')) {
-    res.render('404', { url: req.url });
-    return;
-  }
+	res.status(404);
+	// respond with html page
+	if (req.accepts('html')) {
+		res.render('404', { url: req.url });
+		return;
+	}
 });
 
 app.locals.basedir = path.join(__dirname, '.');
